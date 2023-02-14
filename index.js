d1768b647f9bf55e44c23efac91f2cef18c54aae
melany estevez camata <elibialany@gmail.com>
2023-02-14 12:10:23 -0400

    fixed store

--- index.js
+++ index.js
@@ -2,37 +2,82 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const autoIncrement = require('mongoose-auto-increment');
 // Basic Configuration
-const port = process.env.PORT || 3000;
-
-let i  = 0
-let urls =[]
-
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/public', express.static(`${process.cwd()}/public`));
+const dns = require('dns');
+const URL = require('url').URL;
+//============================================================
+var connection = mongoose.createConnection(process.env.DB_URL);
+autoIncrement.initialize(connection);
+const Schema = mongoose.Schema;
+// Urls
+const urlSchema = new Schema({
+  original_url: { type: String, required: true }
+})
+urlSchema.plugin(autoIncrement.plugin, { model: 'Url', field: 'short_url' })
+let Urls = connection.model('Url', urlSchema)
+//============================================================
+const isValidUrl = urlString => {
+  var urlPattern = new RegExp('^(https?:\\/\\/)?' + // validate protocol
+    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // validate domain name
+    '((\\d{1,3}\\.){3}\\d{1,3}))' + // validate OR ip (v4) address
+    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // validate port and path
+    '(\\?[;&a-z\\d%_.~+=-]*)?' + // validate query string
+    '(\\#[-a-z\\d_]*)?$', 'i'); // validate fragment locator
+  return !!urlPattern.test(urlString);
+}
+app.get('/api/shorturl/:url', async (req, res)=> {
+  const { url } = req.params;
+  if (!url) {
+    return res.json({ error: 'invalid url' })
+  }
+  const findUrl = await Urls.findOne({ short_url: url }).select(['-_id', '-__v']).lean()
+  if (!findUrl){
+    return res.json({ error: 'invalid url' })
+  }
+  res.redirect(findUrl.original_url)
+});
+app.post('/api/shorturl', async (req, res) => {
+  const { url } = req.body;
+  try {
+    if (!isValidUrl(url)) {
+      return res.json({ error: 'invalid url' })
+    }
+    const urlObject = new URL(url);
+    dns.lookup(urlObject.hostname, async (err, address, family) => {
+      if (err) {
+        res.json({
+          original_url: url,
+          short_url: "invalid URL"
+        })
+      } else {
+        let newUrl = await Urls.create({ original_url: url });
+        res.json(
+          {
+            original_url: url,
+            short_url: newUrl.short_url
+          }
+        )
+      }
+    })
+  } catch (error) {
+    console.log(error);
+    return res.json({ error: 'invalid url' })
+  }
+});
+app.get('/a', async (req, res) => {
+  const ans = await Urls.find().exec()
+  console.log(ans);
+  res.send({})
+});
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
-});
-app.post('/api/shorturl',function(req, res) {
-  const url= req.body.url;
-  urls.push({ original_url : url, short_url : i})
-  i=i+1
-  res.json({ original_url : url, short_url : i});
-});
-app.get('/api/shorturl/:url?',function(req, res) {
-  const ans = urls.find(url => url.short_url === url)
-  if (ans) {res.json({ error: 'invalid url' })}
-  res.redirect(ans.original_url)
-});
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
-});
-
-app.listen(port, function() {
-  console.log(`Listening on port ${port}`);
-});
+connection.once('open', () => {
+  console.log('Connection to MongooseDB');
+  app.listen(4000 || process.env.PORT, () => console.log(`Server running on port http://localhost:4000`));
+})
